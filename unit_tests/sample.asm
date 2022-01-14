b1f0c75864aa62ac39c49e1d54a76235cca7c73d
Hyo Jae Shin <hyojaeshin@Jays-MacBook-Pro.local>
2022-01-14 07:00:23 -0800

    simple update

--- unit_tests/sample.asm
+++ unit_tests/sample.asm
@@ -2,18 +2,10 @@
 
 .method $constructor
 const 1
-const 3
-call Int:plus
 const 2
-const 0
+const 1
 call Int:sub
-const 3
-call Int:plus
 call Int:plus
 call Int:print
-pop
-const "
-"
-call String:print
 pop
 return 0
